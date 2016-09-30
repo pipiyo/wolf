0f5b3b5dcb74d97fe9cb9e4ba03beec74b3fb452
pipiyo <fe.cavieres@gmail.com>
2016-09-30 13:30:25 -0300

    ñee

--- index.php
+++ index.php
@@ -3,10 +3,9 @@
 	define('ROOT', realpath(dirname(__FILE__)) . DS);
 	define('URL', "http://local.wolf/");
 	date_default_timezone_set('America/Santiago');
-
+		
 	require_once "Config/Autoload.php";
 	Config\Autoload::run();
 	require_once "Vistas/plantilla.php";
 	Config\Enrutador::run(new Config\Request());
-
 ?>
